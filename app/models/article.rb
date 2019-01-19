ec3cc7f19beb101ae9ad392031f188acbab3b9d7
Anand Raja <meetanandns@gmail.com>
2019-01-19 06:34:57 +0000

    articles table with length validation is done

---
+++ app/models/article.rb
@@ -0,0 +1,5 @@
+class Article < ApplicationRecord
+  validates :title, presence: true, length: {minimum: 3, maximum: 15}
+  validates :description, presence: true, length: {minimum: 10, maximum: 800}
+  
+end
