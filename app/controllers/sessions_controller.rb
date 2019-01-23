0ec8acba8a82f0a0f1d1e086af02893105be21ed
Anand Raja <meetanandns@gmail.com>
2019-01-23 00:33:48 +0000

    login and logout is completed

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,25 @@
+class SessionsController < ApplicationController
+  
+  def new
+  
+  end
+  
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      flash[:success] = "You have logged in successfully"
+      redirect_to user_path(user)
+    else
+      flash.now[:warning] = "There is something wrong with your credentials"
+      render 'new'
+    end
+  end
+  
+  def destroy
+    session[:user_id] = nil
+    flash[:success] = "You have logged out successfully!"
+    redirect_to root_path
+  end
+  
+end
