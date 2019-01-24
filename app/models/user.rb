0b7957389b3014d63061e56b46201c0777037aa5
Anand Raja <meetanandns@gmail.com>
2019-01-24 12:30:30 +0000

    email length is increased

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true,
             uniqueness: {case_sensitive: false},
-            length: {minimum: 5, maximum: 25},
+            length: {minimum: 5, maximum: 50},
             format: {with: VALID_EMAIL_REGEX}
   has_secure_password
             
