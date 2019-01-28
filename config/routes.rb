1dde4cab2d7b726899e623ba2e99ae5a8c920dee
Anand Raja <meetanandns@gmail.com>
2019-01-28 14:44:06 +0000

    testing with categories added

--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,5 @@ Rails.application.routes.draw do
   delete 'logout', to: 'sessions#destroy'
   get 'ss7', to: 'codes#index'
   resources :codes, except: [:index]
+  resources :categories, except: [:destroy]
 end
