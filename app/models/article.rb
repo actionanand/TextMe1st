57ae1dfe8a8294461c2ebd73683662d252db2536
Anand Raja <meetanandns@gmail.com>
2019-01-29 11:52:04 +0000

    categories integerated with articles

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,8 @@
 class Article < ApplicationRecord
   
   belongs_to :user
+  has_many :article_categories
+  has_many :categories, through: :article_categories
   validates :user_id, presence: true
   validates :title, presence: true, length: {minimum: 5, maximum: 50}
   validates :description, presence: true, length: {minimum: 13, maximum: 1300}
