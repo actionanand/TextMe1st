6d35ce0a7919cba140692731439dcd3cff162132
Anand Raja <meetanandns@gmail.com>
2019-02-02 19:48:17 +0000

    show password facility

---
+++ app/assets/javascripts/cusom.js
@@ -0,0 +1,19 @@
+// toggle password visibility
+$( document ).ready(function() {
+
+  $(".reveal").on('click',function() {
+    
+    var $pwd = $(".pwd");
+    
+    $(this).find('i').toggleClass('glyphicon-eye-open').toggleClass('glyphicon-eye-close');
+    if ($pwd.attr('type') === 'password')
+      {
+      $pwd.attr('type', 'text');
+      } 
+    else {
+      $pwd.attr('type', 'password');
+    }
+    
+  });
+
+});
