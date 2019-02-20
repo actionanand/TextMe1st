d5d35a14aa3ab1489b178b3029a763e7c9c38dde
Anand Raja <meetanandns@gmail.com>
2019-02-20 07:09:39 +0000

    Comments with Actioncable ready for prod

--- Gemfile
+++ Gemfile
@@ -61,4 +61,5 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 group :production do
     gem 'pg'
+    gem 'redis'
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,4 +83,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  
+  config.web_socket_server_url = "wss://textme1st.herokuapp.com/cable"
+  config.action_cable.allowed_request_origins = ['https://textme1st.herokuapp.com','http://textme1st.herokuapp.com/']
 end
