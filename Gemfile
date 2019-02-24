a3c1de3e996e8f4893dea136a70908d930a11431
Anand Raja <meetanandns@gmail.com>
2019-02-24 18:25:03 +0000

    View password without gem

--- Gemfile
+++ Gemfile
@@ -30,6 +30,7 @@ gem 'bootstrap-will_paginate', '1.0.0'
 gem 'jquery-rails'
 # Turbolinks makes navigating your web application faster. Read more: https://github.com/turbolinks/turbolinks
 gem 'turbolinks', '~> 5'
+gem 'jquery-turbolinks'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
 gem 'jbuilder', '~> 2.5'
 gem 'rails-controller-testing'
--- app/assets/javascripts/cusom.js
+++ app/assets/javascripts/cusom.js
@@ -1,19 +1,38 @@
 // toggle password visibility
-$( document ).ready(function() {
-
-  $(".reveal").on('click',function() {
-    
-    var $pwd = $(".pwd");
-    
-    $(this).find('i').toggleClass('glyphicon-eye-open').toggleClass('glyphicon-eye-close');
-    if ($pwd.attr('type') === 'password')
+$(document).on('turbolinks:load', function() 
       {
-      $pwd.attr('type', 'text');
-      } 
-    else {
-      $pwd.attr('type', 'password');
-    }
-    
-  })
-
-});
+         $("#showhide").click(function() 
+         {
+            if ($(this).data('val') == "1") 
+            {
+               $("#pwd").prop('type','text');
+               $("#eye").attr("class","glyphicon glyphicon-eye-close");
+               $(this).data('val','0');
+            }
+            else
+            {
+               $("#pwd").prop('type', 'password');
+               $("#eye").attr("class","glyphicon glyphicon-eye-open");
+               $(this).data('val','1');
+            }
+         });
+      });
+      
+$(document).on('turbolinks:load', function() 
+      {
+         $("#showhide1").click(function() 
+         {
+            if ($(this).data('val') == "1") 
+            {
+               $("#pwd1").prop('type','text');
+               $("#eye1").attr("class","glyphicon glyphicon-eye-close");
+               $(this).data('val','0');
+            }
+            else
+            {
+               $("#pwd1").prop('type', 'password');
+               $("#eye1").attr("class","glyphicon glyphicon-eye-open");
+               $(this).data('val','1');
+            }
+         });
+      });
